c932f1f2e292f3981a648fe674ef70ae2f814461
Oualid KHIAL <31378416+oualidor@users.noreply.github.com>
2020-08-25 13:37:19 +0200

    Add files via upload

---
+++ youritdepartment/customApis/java/functions.js
@@ -0,0 +1,82 @@
+
+
+function setDimenstions(){
+
+  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+
+
+    if(w>1100){
+
+
+    }else{
+      w=1100;
+    }
+
+    if(h>500){
+
+    }else{
+      h=500;
+    }
+  $('body').css("height", h+"px");
+  $('body').css("width", w+"px");
+}
+
+function setMobileDimensions(){
+
+  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  $('body').css("height", h+"px");
+  $('body').css("width", w+"px");
+}
+
+function test(){
+  $('.homeText').textillate({
+    minDisplayTime: 200,
+    in: { effect: 'flipInX', sync: true },
+    out :{  delay: 1, effect: 'tada', sync: true},
+    loop: true
+  });
+}
+$(document).ready(function(){
+
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (isMobile) {
+    setMobileDimensions();
+    $(window).resize(function(){
+      setMobileDimensions();
+    });
+
+
+  }else {
+    setDimenstions();
+
+    $(window).resize(function(){
+      setDimenstions();
+    });
+  }
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  })
+});
---
+++ youritdepartment/precache-manifest.56ebb61c74f0d07136ea978f45b995d4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f8ea0bda7bf2db550a032bb9c1dcdb",
+    "url": "/youritdepartment/index.html"
+  },
+  {
+    "revision": "765d9de9064799097c0c",
+    "url": "/youritdepartment/static/css/main.1af4fe5d.chunk.css"
+  },
+  {
+    "revision": "84aaea8a0794417e9caf",
+    "url": "/youritdepartment/static/js/2.666e74fd.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/youritdepartment/static/js/2.666e74fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765d9de9064799097c0c",
+    "url": "/youritdepartment/static/js/main.37e2c624.chunk.js"
+  },
+  {
+    "revision": "b4bd802d01d8c99c606d",
+    "url": "/youritdepartment/static/js/runtime-main.20255d09.js"
+  },
+  {
+    "revision": "ccc27e88b7f24393db3cd2fe3a2cbf47",
+    "url": "/youritdepartment/static/media/App-development.ccc27e88.png"
+  },
+  {
+    "revision": "9336ed4a8234d2841a0bd5ad3b0e7978",
+    "url": "/youritdepartment/static/media/aboutUS2.9336ed4a.gif"
+  },
+  {
+    "revision": "12a6bdfe544206eb174e94c994429199",
+    "url": "/youritdepartment/static/media/bg.12a6bdfe.svg"
+  },
+  {
+    "revision": "23f4fce9fce11a0d30e9e5dcc14dd454",
+    "url": "/youritdepartment/static/media/brain.23f4fce9.png"
+  },
+  {
+    "revision": "c504a8e087c5c5114104e3d98db8beed",
+    "url": "/youritdepartment/static/media/ecomerce1.c504a8e0.jpg"
+  },
+  {
+    "revision": "da8d70ceda4d34aa640175e86460720e",
+    "url": "/youritdepartment/static/media/ecomerce2.da8d70ce.jpg"
+  },
+  {
+    "revision": "8168313ad1886a7650a205f5500086f3",
+    "url": "/youritdepartment/static/media/logo.8168313a.png"
+  },
+  {
+    "revision": "8e3ea14fd3ad5b05f15e19940bb16eca",
+    "url": "/youritdepartment/static/media/mimmar.8e3ea14f.png"
+  },
+  {
+    "revision": "00c32d223683a25f4e3df52f4704de1f",
+    "url": "/youritdepartment/static/media/next.00c32d22.webp"
+  },
+  {
+    "revision": "940ec925f190e7aeacb3061f6c39c475",
+    "url": "/youritdepartment/static/media/oualidPorfolio.940ec925.png"
+  },
+  {
+    "revision": "b6b7c3559a47b9ca12a4d9b57b9e800d",
+    "url": "/youritdepartment/static/media/previous.b6b7c355.webp"
+  },
+  {
+    "revision": "fdd3432e54c04a0a94e98b7fb7bfe846",
+    "url": "/youritdepartment/static/media/wordpress.fdd3432e.png"
+  }
+]);
