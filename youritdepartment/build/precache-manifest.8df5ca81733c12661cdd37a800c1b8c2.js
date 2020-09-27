676bc87e52811338ca28c806c7a02f212db566bb
oualid KHIAL <walid.khial@gmail.com>
2020-09-27 22:35:07 +0100

    Normal commit

---
+++ youritdepartment/build/precache-manifest.8df5ca81733c12661cdd37a800c1b8c2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9343d6575935fc34f79f3c480ee9b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec911d6eccabe037fc5",
+    "url": "/static/css/main.c27a09cc.chunk.css"
+  },
+  {
+    "revision": "13feec6266c16a5f6472",
+    "url": "/static/js/2.9ca438d6.chunk.js"
+  },
+  {
+    "revision": "845934a17105fe9618de33609e765621",
+    "url": "/static/js/2.9ca438d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec911d6eccabe037fc5",
+    "url": "/static/js/main.c2961c0f.chunk.js"
+  },
+  {
+    "revision": "26c663f71b3f46c75528",
+    "url": "/static/js/runtime-main.f91f35ce.js"
+  },
+  {
+    "revision": "ccc27e88b7f24393db3cd2fe3a2cbf47",
+    "url": "/static/media/App-development.ccc27e88.png"
+  },
+  {
+    "revision": "8c44794e80a92d2d858682c1a10e7551",
+    "url": "/static/media/aboutUs2.8c44794e.png"
+  },
+  {
+    "revision": "d5c398a63b35dd1320e5cda683a181bf",
+    "url": "/static/media/arch.d5c398a6.png"
+  },
+  {
+    "revision": "23f4fce9fce11a0d30e9e5dcc14dd454",
+    "url": "/static/media/brain.23f4fce9.png"
+  },
+  {
+    "revision": "1b71c1611e9e222e6c1e99f741382907",
+    "url": "/static/media/contactUs2.1b71c161.png"
+  },
+  {
+    "revision": "c504a8e087c5c5114104e3d98db8beed",
+    "url": "/static/media/ecomerce1.c504a8e0.jpg"
+  },
+  {
+    "revision": "da8d70ceda4d34aa640175e86460720e",
+    "url": "/static/media/ecomerce2.da8d70ce.jpg"
+  },
+  {
+    "revision": "079a1b5400c1bb790d5ca95d1c124776",
+    "url": "/static/media/logoBlue.079a1b54.svg"
+  },
+  {
+    "revision": "09241bf5d1a943a01aa7e240a8be5996",
+    "url": "/static/media/logoWhite.09241bf5.svg"
+  },
+  {
+    "revision": "300fa89652074bdcff7bc149aee72cff",
+    "url": "/static/media/mainBackGround.300fa896.png"
+  },
+  {
+    "revision": "8e3ea14fd3ad5b05f15e19940bb16eca",
+    "url": "/static/media/mimmar.8e3ea14f.png"
+  },
+  {
+    "revision": "00c32d223683a25f4e3df52f4704de1f",
+    "url": "/static/media/next.00c32d22.webp"
+  },
+  {
+    "revision": "940ec925f190e7aeacb3061f6c39c475",
+    "url": "/static/media/oualidPorfolio.940ec925.png"
+  },
+  {
+    "revision": "b6b7c3559a47b9ca12a4d9b57b9e800d",
+    "url": "/static/media/previous.b6b7c355.webp"
+  },
+  {
+    "revision": "d290a11ff7a0fca25d58c7ce590f7fb6",
+    "url": "/static/media/scrollDown.d290a11f.gif"
+  },
+  {
+    "revision": "83ccc4cbb4457049298e0947b644ae6b",
+    "url": "/static/media/webSitesDev.83ccc4cb.png"
+  },
+  {
+    "revision": "fdd3432e54c04a0a94e98b7fb7bfe846",
+    "url": "/static/media/wordpress.fdd3432e.png"
+  }
+]);
